564afe7a4fe9a6c148c45e9bf8161acf3c1799ad
Arnol Plazas <plazasarnol@gmail.com>
2023-02-21 06:33:25 -0500

    packing files with password used zip

---
+++ 30_zipPassword.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+# Programa para ejemplificar el empaquetamiento con clave utilizando zip.
+# Autor: Arnol Plazas
+
+echo "Empaquetar todos los scripts de la carpeta shell con zip y asignarle una clave de seguridad"
+
+zip -e shellCourse.zip *.sh
